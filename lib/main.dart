7c06ccfa9208684bdf6f249a4f2bc929a246d11e
Christian Georgeson <pcw.georgeson@gmail.com>
2019-08-01 13:38:48 +0200

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Color(0xFF212121),
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.red[400],
         ),
         body: DicePage(),
       ),
@@ -15,9 +15,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+           Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  setState(() {
+                   leftDiceNumber = 6; 
+                   print('diceNumber = $leftDiceNumber');
+                  });
+                },
+                child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+          
+          ),
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  print('Right Button is pressed');
+                },
+                child: Image(
+                image: AssetImage('images/dice1.png'),
+                ),
+              ),
+          ),
+        ]
+      ),
+    );
   }
 }
